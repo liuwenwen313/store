ea1a3bc3f5bb9d0525554ebb815da392a1a878be
unknown <1798032538@qq.com>
2021-04-21 18:21:00 +0800

    第三次提交

---
+++ 变量服装销售占比.py
@@ -0,0 +1,275 @@
+#1号日期服装
+id1 = 1
+name1 = "羽绒服"
+price1 = 253.6
+num1 = 500
+sv1  =10
+#2号日期服装
+id2 =2
+name2 = "牛仔裤"
+price2 = 86.3
+num2 = 600
+sv2  =60
+#3号日期服装
+id3 =3
+name3 = "风衣"
+price3 = 96.8
+num3 = 335
+sv3  =43
+#4号日期服装
+id4 =4
+name4 = "皮草"
+price4 = 135.9
+num4 = 855
+sv4  =63
+#5号日期服装
+id5 =5
+name5 = "T恤"
+price5 = 65.8
+num5 = 632
+sv5  =63
+#6号日期服装
+id6 =6
+name6 = "衬衫"
+price6 = 49.3
+num6 = 562
+sv6  =120
+#7号日期服装
+id7 =7
+name7 = "牛仔裤"
+price7 = 86.3
+num7 = 600
+sv7  =72
+#8号日期服装
+id8 =8
+name8 = "羽绒服"
+price8 = 253.6
+num8 = 500
+sv8  =69
+#9号日期服装
+id9 =9
+name9 = "牛仔裤"
+price9 = 86.3
+num9 = 600
+sv9  =35
+#10号日期服装
+id10 =10
+name10 = "羽绒服"
+price10 = 253.6
+num10 = 500
+sv10  =140
+#11号日期服装
+id11 =11
+name11 = "牛仔裤"
+price11 = 86.3
+num11 = 600
+sv11  =90
+#12号日期服装
+id12 =12
+name12 = "皮草"
+price12 = 135.9
+num12 = 855
+sv12  =24
+#13号日期服装
+id13 =13
+name13 = "T恤"
+price13 = 65.8
+num13= 632
+sv13  =45
+#14号日期服装
+id14 =14
+name14 = "风衣"
+price14 = 96.8
+num14 = 335
+sv14  =25
+#15号日期服装
+id15 =15
+name15 = "牛仔裤"
+price15 = 86.3
+num15= 600
+sv15  =60
+#16号日期服装
+id16 =16
+name16 = "T恤"
+price16 = 65.8
+num16 = 632
+sv16  =129
+#17号日期服装
+id17 =17
+name17 = "羽绒服"
+price17 = 253.6
+num17 = 500
+sv17  =10
+#18号日期服装
+id18 =18
+name18 = "风衣"
+price18 = 96.8
+num18 = 335
+sv18  =43
+#19号日期服装
+id19 =19
+name19 = "T恤"
+price19 = 65.8
+num19 = 632
+sv19 =63
+#20号日期服装
+id20 =20
+name20 = "牛仔裤"
+price20 = 86.3
+num20 = 600
+sv20 =60
+#21号日期服装
+id21 =21
+name21 = "皮草"
+price21 = 135.9
+num21 = 855
+sv21  =63
+#22号日期服装
+id22 =22
+name22 = "风衣"
+price22 = 96.8
+num22 = 335
+sv22  =60
+#23号日期服装
+id23 =23
+name23 = "T恤"
+price23 = 65.8
+num23 = 632
+sv23  =58
+#24号日期服装
+id24 =24
+name24 = "牛仔裤"
+price24 = 86.3
+num24 = 600
+sv24  =140
+#25号日期服装
+id25 =25
+name25 = "T恤"
+price25 = 65.8
+num25 = 632
+sv25  =48
+#26号日期服装
+id26 =26
+name26 = "风衣"
+price26 = 96.8
+num26 = 335
+sv26  =43
+#27号日期服装
+id27 =27
+name27 = "皮草"
+price27 = 135.9
+num27 = 855
+sv27  =57
+#28号日期服装
+id28 =28
+name28 = "羽绒服"
+price28 = 253.6
+num28 = 500
+sv28  =10
+#29号日期服装
+id29 =29
+name29 = "T恤"
+price29 = 65.8
+num29= 632
+sv29  =63
+#30号日期服装
+id30 =30
+name30 = "风衣"
+price30 = 96.8
+num30 = 335
+sv30  =78
+
+print("---------------------------欢迎来到衣服商城————————————————————————-")
+print("日期\t\t\t服装名称\t\t价格\t\t\t库存数量\t\t\t销售量")
+print(id1,"\t\t\t",name1,"\t\t",price1,"\t\t",num1,"\t\t\t",sv1)
+print(id2,"\t\t\t",name2,"\t\t",price2,"\t\t",num2,"\t\t\t",sv2)
+print(id3,"\t\t\t",name3,"\t\t",price3,"\t\t",num3,"\t\t\t",sv3)
+print(id4,"\t\t\t",name4,"\t\t",price4,"\t\t",num4,"\t\t\t",sv4)
+print(id5,"\t\t\t",name5,"\t\t",price5,"\t\t",num5,"\t\t\t",sv5)
+print(id6,"\t\t\t",name6,"\t\t",price6,"\t\t",num6,"\t\t\t",sv6)
+print(id7,"\t\t\t",name7,"\t\t",price7,"\t\t",num7,"\t\t\t",sv7)
+print(id8,"\t\t\t",name8,"\t\t",price8,"\t\t",num8,"\t\t\t",sv8)
+print(id9,"\t\t\t",name9,"\t\t",price9,"\t\t",num9,"\t\t\t",sv9)
+print(id10,"\t\t\t",name10,"\t\t",price10,"\t\t",num10,"\t\t\t",sv10)
+print(id11,"\t\t\t",name11,"\t\t",price11,"\t\t",num11,"\t\t\t",sv11)
+print(id12,"\t\t\t",name12,"\t\t",price12,"\t\t",num12,"\t\t\t",sv12)
+print(id13,"\t\t\t",name13,"\t\t",price13,"\t\t",num13,"\t\t\t",sv13)
+print(id14,"\t\t\t",name14,"\t\t",price14,"\t\t",num14,"\t\t\t",sv14)
+print(id15,"\t\t\t",name15,"\t\t",price15,"\t\t",num15,"\t\t\t",sv15)
+print(id16,"\t\t\t",name16,"\t\t",price16,"\t\t",num16,"\t\t\t",sv16)
+print(id17,"\t\t\t",name17,"\t\t",price17,"\t\t",num17,"\t\t\t",sv17)
+print(id18,"\t\t\t",name18,"\t\t",price18,"\t\t",num18,"\t\t\t",sv18)
+print(id19,"\t\t\t",name19,"\t\t",price19,"\t\t",num19,"\t\t\t",sv19)
+print(id20,"\t\t\t",name20,"\t\t",price20,"\t\t",num20,"\t\t\t",sv20)
+print(id21,"\t\t\t",name21,"\t\t",price21,"\t\t",num21,"\t\t\t",sv21)
+print(id22,"\t\t\t",name22,"\t\t",price22,"\t\t",num22,"\t\t\t",sv22)
+print(id23,"\t\t\t",name23,"\t\t",price23,"\t\t",num23,"\t\t\t",sv23)
+print(id24,"\t\t\t",name24,"\t\t",price24,"\t\t",num24,"\t\t\t",sv24)
+print(id25,"\t\t\t",name25,"\t\t",price25,"\t\t",num25,"\t\t\t",sv25)
+print(id26,"\t\t\t",name26,"\t\t",price26,"\t\t",num26,"\t\t\t",sv26)
+print(id27,"\t\t\t",name27,"\t\t",price27,"\t\t",num27,"\t\t\t",sv27)
+print(id28,"\t\t\t",name28,"\t\t",price28,"\t\t",num28,"\t\t\t",sv28)
+print(id29,"\t\t\t",name29,"\t\t",price29,"\t\t",num29,"\t\t\t",sv29)
+print(id30,"\t\t\t",name30,"\t\t",price30,"\t\t",num30,"\t\t\t",sv30)
+print("------------------------------------------------------------")
+#z = "总销售额"
+#ZL= "月总销售量"
+#zy ="羽绒服总销售量"
+#ye="羽绒服总销售额"
+#a=羽绒服销售量占比
+#a2=羽绒服销售额占比
+#zn ="牛仔裤总销售量"
+#ne =“牛仔裤总销售额”
+#b ="牛仔裤销售量占比"
+#b1 =“牛仔裤总销售额占比”
+#zf ="风衣总销售量"
+#fe=“风衣总销售额”
+#c ="风衣总销售量占比"
+#c1=“风衣总销售额占比”
+#pc ="皮草总销售量"
+#pe="皮草总销售额"
+#d ="皮草总销售量占比"
+#d1="皮草总销售额占比"
+#tz ="T恤总销售量"
+#te ="T恤总销售额"
+#e ="T恤总销售量占比"
+#e1 ="T恤总销售额占比"
+#zc ="衬衫总销售量"
+#ce ="衬衫总销售额"
+#f ="衬衫总销售量占比"
+#f1 ="衬衫总销售额占比"
+zy=sv1+sv8+sv10+sv17+sv28
+ye=zy*price1
+zn=sv2+sv7+sv9+sv11+sv15+sv20+sv24
+ne=zn*price2
+zf=sv3+sv14+sv18+sv22+sv26+sv30
+fe=zf*price3
+pc=sv4+sv12+sv21+sv27
+pe=pc*price4
+tz=sv5+sv13+sv16+sv19+sv23+sv25+sv29
+te=tz*price5
+zc=sv6
+ce=zc*price6
+ZL=zy+zn+zf+pc+tz+zc
+Z=ye+ne+fe+pe+te+ce
+a=zy/ZL*100
+a1=ye/Z*100
+b=zn/ZL*100
+b1=ne/Z*100
+c=zf/ZL*100
+c1=fe/Z*100
+d=pc/ZL*100
+d1=pe/Z*100
+e=tz/ZL*100
+e1=te/Z*100
+f=zc/ZL*100
+f1=ce/Z*100
+print("------------------销售统计表-----------------------------")
+print("服装名称\t\t月销售量\t\t\t月销售额\t\t\t\t销售量占比\t\t\t销售额占比")
+print(id1,"\t\t\t",zy,"\t\t\t",ye,"\t\t\t",round(a,2),"%\t\t\t",round(a1,2),"%")
+print(id2,"\t\t\t",zn,"\t\t\t",ne,"\t\t\t",round(b,2),"%\t\t\t",round(b1,2),"%")
+print(id3,"\t\t\t",zf,"\t\t\t",fe,"\t\t\t",round(c,2),"%\t\t\t",round(c1,2),"%")
+print(id4,"\t\t\t",pc,"\t\t\t",round(pe,2),"\t\t\t",round(d,2),"%\t\t\t",round(d1,2),"%")
+print(id5,"\t\t\t",tz,"\t\t\t",round(te,2),"\t\t\t",round(e,2),"%\t\t\t",round(e1,2),"%")
+print(id6,"\t\t\t",zc,"\t\t\t",ce,"\t\t\t",round(f,2),"%\t\t\t\t",round(f1,2),"%")
+print("本月总销售额为",round(Z,2))
